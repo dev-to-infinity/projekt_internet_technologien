c22c528190c4a2a8c157d7099e6314697aaf963a
jr14960 <jakow.rese@stud.th-deg.de>
2026-05-22 11:33:20 +0200

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const picture_A = document.getElementById("picture_A")
+const picture_B = document.getElementById("picture_B")
+const picture_C = document.getElementById("picture_C")
+picture_A.onclick = () => {document.getElementById("home_about_right").innerHTML = '<img src="C:/Users/jakow/Documents/THD/Unterrichtsmaterialien/Internet_Technologien/Final/Img/Airport.jpg" alt="Airport" id="sec_image"></img>'}
+picture_B.onclick = () => {document.getElementById("home_about_right").innerHTML = '<img src="C:/Users/jakow/Documents/THD/Unterrichtsmaterialien/Internet_Technologien/Final/Img/Night.jpg" alt="Nightview"></img>'}
+picture_C.onclick = () => {document.getElementById("home_about_right").innerHTML = '<img src="C:/Users/jakow/Documents/THD/Unterrichtsmaterialien/Internet_Technologien/Final/Img/Inside.jpg" alt="Interior"></img>'}
